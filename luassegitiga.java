bf8779f42902c35d677d697d3dcbe2659ad01520
Bobby <MrBobbyGustafyan@gmail.com>
2017-02-09 09:23:24 +0700

    commit k6

---
+++ luassegitiga.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class luassegitiga {
+public static void main(String[] args) {
+   int alas;
+   int tinggi;
+   double luas;
+
+   Scanner scan = new Scanner(System.in);
+   System.out.print("Masukan Alas: ");
+   alas = scan.nextInt();
+   System.out.print("Masukan Tinggi: ");
+   tinggi = scan.nextInt();
+  
+   luas = ((alas * tinggi)/2);
+   System.out.println("Luas Segitiga = " +luas);
+    }
+}
+
